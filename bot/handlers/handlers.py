fbcea3dff15f314ffebe8da402c2e59c0615b3d0
Nikita <nikita.gubarev31@gmail.com>
2023-01-30 21:13:30 +0200

    Add the searching message before handler answer

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -12,15 +12,11 @@ from .parsing import get_joke_text, get_mem_image
 logger = logging.getLogger()
 
 
-async def _send_error_message(message: types.Message) -> None:
-    """For sending to user the error message"""
-    await message.answer("Упс, возникла ошибка, повторите попытку :)")
-
-
 @DP.message_handler(Text("Анекдот", ignore_case=True))
 async def joke_handler(message: types.Message) -> None:
     """The handler for sending a joke"""
     try:
+        await _send_searching_message(message)
         await message.answer(get_joke_text())
     except Exception as e:
         await _send_error_message(message)
@@ -32,8 +28,19 @@ async def joke_handler(message: types.Message) -> None:
 async def mem_handler(message: types.Message) -> None:
     """The handler for sending a mem image"""
     try:
+        await _send_searching_message(message)
         await message.answer_photo(get_mem_image())
     except Exception as e:
         await _send_error_message(message)
         logger.error(f"Exception in mem handler: {str(e)}")
     await menu(message)
+
+
+async def _send_searching_message(message: types.Message) -> None:
+    """For sending to user the searching message"""
+    await message.answer("Поиск...")
+
+
+async def _send_error_message(message: types.Message) -> None:
+    """For sending to user the error message"""
+    await message.answer("Упс, возникла ошибка, повторите попытку :)")
